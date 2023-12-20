7ac7c43b8186c589362101d729c9d2b10efd9575
areejtechcampus <areej262626@gmail.com>
2023-12-20 00:42:40 -0800

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - prints the value and return 0
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+printf("_putchar\n")
+return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This readme for the function nested loops
